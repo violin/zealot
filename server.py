8eebf3e90b9e3cc17debdf61d261c582e6c7870c
杨舟 <hzyangzhou@corp.netease.com>
2015-11-05 11:00:25 +0800

    decimal

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import web
 import db
 import json
 import sys,os
+import decimal
 from datetime import *
 #import openid
 import tempfile
@@ -117,6 +118,8 @@ class CJsonEncoder(json.JSONEncoder):
             return obj.strftime('%Y-%m-%d %H:%M:%S')
         elif isinstance(obj, date):
             return obj.strftime('%Y-%m-%d')
+        elif isinstance(obj, decimal.Decimal):
+            return float(obj)
         else:
             return json.JSONEncoder.default(self, obj)
 
